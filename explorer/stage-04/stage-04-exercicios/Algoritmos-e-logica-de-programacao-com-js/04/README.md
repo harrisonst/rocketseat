48e1687e045e4550421f0bd9f387c878e2c8d6ea
harrisonst <harrisonsantosdev@gmail.com>
2022-10-14 23:32:35 -0300

    exercicio de array

---
+++ explorer/stage-04/stage-04-exercicios/Algoritmos-e-logica-de-programacao-com-js/04/README.md
@@ -0,0 +1,16 @@
+# Estrutura de dados
+
+- Organização e gerenciamento dos dados
+- Melhor eficiência do código
+
+[] Estuturas de repetição
+[] Vetores (Arrays)
+
+
+### Vídeo: Criando lista de compras
+
+
+
+
+
+
---
+++ explorer/stage-04/stage-04-exercicios/Algoritmos-e-logica-de-programacao-com-js/04/script.js
@@ -0,0 +1,8 @@
+/*
+  Capture 10 itens para compor a lista de um supermercado.
+  Após capturá-los, imprima-os separando por vírgula.
+ */
+
+  let listaDeCompras = ["iogurte" , "aveia", "azeite" , "queijo", "café", "chá", "vinho", "frutas", "lentilha" ,"pão"];
+
+  
