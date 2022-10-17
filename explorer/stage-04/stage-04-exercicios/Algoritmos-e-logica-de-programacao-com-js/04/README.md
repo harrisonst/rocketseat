396013134b2ea85ed9b9f8cb0f2a1f2944a08bb0
harrisonst <harrisonsantosdev@gmail.com>
2022-10-17 04:22:27 -0300

    data structure, Array, for

--- explorer/stage-04/stage-04-exercicios/Algoritmos-e-logica-de-programacao-com-js/04/README.md
+++ explorer/stage-04/stage-04-exercicios/Algoritmos-e-logica-de-programacao-com-js/04/README.md
@@ -1,13 +1,20 @@
 # Estrutura de dados
 
+### Vídeo: Criando lista de compras
+
 - Organização e gerenciamento dos dados
 - Melhor eficiência do código
 
 [] Estuturas de repetição
+  - laço de repetição
+  - for
 [] Vetores (Arrays)
+  [] Tipo de dado estruturado
+[] incremental (++): o valor +1
+[] Tipo de dado: undefined
 
 
-### Vídeo: Criando lista de compras
+
 
 
 
--- explorer/stage-04/stage-04-exercicios/Algoritmos-e-logica-de-programacao-com-js/04/script.js
+++ explorer/stage-04/stage-04-exercicios/Algoritmos-e-logica-de-programacao-com-js/04/script.js
@@ -3,6 +3,33 @@
   Após capturá-los, imprima-os separando por vírgula.
  */
 
-  let listaDeCompras = ["iogurte" , "aveia", "azeite" , "queijo", "café", "chá", "vinho", "frutas", "lentilha" ,"pão"];
+// A solução abaixo é inviável em função da quantidade de linhas a serem modificadas numa eventual manutenção
+// let product1 = prompt('Digite o produto 1')
+// let product2 = prompt('Digite o produto 2')
+// let product3 = prompt('Digite o produto 3')
+// let product4 = prompt('Digite o produto 4')
+// let product5 = prompt('Digite o produto 5')
+// let product6 = prompt('Digite o produto 6')
+// let product7 = prompt('Digite o produto 7')
+// let product8 = prompt('Digite o produto 8')
+// let product9 = prompt('Digite o produto 9')
+// let product10 = prompt('Digite o produto 10')
+
+// alert('Os produtos digitados foram ' + product1 + ', ' + product2 + ', ' + product3 + ', ' + product4 + ', ' + 
+// product5 + ', ' + product6 + ', ' + product7 + ', ' + product8 + ', ' + product9 + 'e ' + product10 + '.')
+let itens = [];
+
+  for (let item = 0; item < 10; item++){
+  let intemName = prompt('Digite o item' + (item + 1))
+
+  itens[item]= intemName
+}
+
+alert(itens)
+
+
+
+  
+
 
   
