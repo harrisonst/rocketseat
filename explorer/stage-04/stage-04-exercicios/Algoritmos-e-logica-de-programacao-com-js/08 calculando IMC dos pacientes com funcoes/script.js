41dda6b7837f9f6fd668f0f899923d5ef3402098
harrisonst <harrisonsantosdev@gmail.com>
2022-10-25 17:41:04 -0300

    stage04 IMC

--- explorer/stage-04/stage-04-exercicios/Algoritmos-e-logica-de-programacao-com-js/08 calculando IMC dos pacientes com funcoes/script.js
+++ explorer/stage-04/stage-04-exercicios/Algoritmos-e-logica-de-programacao-com-js/08 calculando IMC dos pacientes com funcoes/script.js
@@ -1,26 +1,48 @@
-/*
-Dada uma lista de pacientes, descubra o IMC de cada paciente e imprima
-"Paciente X possui o IMC de: Y"
+/* 
+  Dada uma lista de pacientes, descubra o IMC de cada paciente e imprima
 
-Onde X é o nome do paciente e Y é o IMC desse paciente
+  "Paciente X possui o IMC de: Y"
 
-Crie uma função para fazer o cálculo de IMC
+  Onde X é o nome do paciente e Y é o IMC desse paciente
+
+  Crie uma função para fazer o cálculo de IMC
 */
 
-// peso/(altura * altura)
+/* peso / (altura * altura) */
 
 const patients = [
   {
-    patientName: 'Joana',
-    age: 29,
-    weight: 70,
-    height: 175,
+    name: "Luiz",
+    age: 20,
+    weight: 100,
+    height: 190
   },
   {
-    patientName: 'Carlos',
-    age: 35,
-    weight: 80,
-    height: 175,
-  },  
+    name: "Alexandra",
+    age: 27,
+    weight: 70,
+    height: 170
+  },
+  {
+    name: "Carlos",
+    age: 42,
+    weight: 90,
+    height: 180
+  },
 ]
 
+function IMC(weight, height) {
+  return (weight / ((height / 100) ** 2)).toFixed(2)
+}
+
+function printPatientIMC(patient) {
+  return `
+    Paciente ${patient.name} possui o IMC de
+    ${IMC(patient.weight, patient.height)}
+  `
+}
+
+for (let patient of patients) {
+  let IMCmessage = printPatientIMC(patient)
+  alert(IMCmessage)
+}
