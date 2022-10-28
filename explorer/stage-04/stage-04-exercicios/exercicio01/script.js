e0c8cc8d4afdc6fcf1422708799be477f8be2962
harrisonst <harrisonsantosdev@gmail.com>
2022-10-27 23:15:30 -0300

    exercice01-concluded

--- explorer/stage-04/stage-04-exercicios/exercicio01/script.js
+++ explorer/stage-04/stage-04-exercicios/exercicio01/script.js
@@ -6,8 +6,8 @@ number1 = Number(number1)
 number2 = Number(number2)
 
 alert(`A soma dos dois números é ${number1 + number2}`)
-alert (`A subtração dos dois números é ${number1 - number2}`)
-alert (`A multiplicação dos dois números é ${number1 * number2}`)
+alert(`A subtração dos dois números é ${number1 - number2}`)
+alert(`A multiplicação dos dois números é ${number1 * number2}`)
 alert(`A divisão dos dois números é ${(number1 / number2).toFixed(2)}`)
 alert(`O resto da divisão dos dois números é ${number1 % number2}`)
 
@@ -18,8 +18,8 @@ if (number1 % number2 === 0) {
 }
 
 if (number1 === number2) {
-  alert (`Os dois números são iguais`)
+  alert(`Os dois números são iguais`)
 } else {
-  alert (`Os dois números são diferentes`)
+  alert(`Os dois números são diferentes`)
 }
 
