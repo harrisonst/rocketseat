cb647a26226b8f9fbd5a694ee62fcb9dbe428499
harrisonst <harrisonsantosdev@gmail.com>
2023-08-10 00:14:11 -0300

    up date

---
+++ explorer/stage-04/stage-04-exercicios/intensivao/intensivao js/2/main.js
@@ -0,0 +1,8 @@
+//Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+
+let numberOne = prompt("Insira um número na primeira variável")
+let numberTwo = prompt("Insira um número na segunda variável")
+
+result = Number(numberOne) + Number(numberTwo) 
+
+alert(`O resultado da soma entre as duas variáveis é ${result}.`)
