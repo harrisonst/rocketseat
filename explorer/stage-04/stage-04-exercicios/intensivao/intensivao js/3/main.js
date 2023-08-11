81b49b56d1b39661088bec898a304e5a8a514086
harrisonst <harrisonsantosdev@gmail.com>
2023-08-11 08:11:41 -0300

    intensivao js ex3

--- explorer/stage-04/stage-04-exercicios/intensivao/intensivao js/3/main.js
+++ explorer/stage-04/stage-04-exercicios/intensivao/intensivao js/3/main.js
@@ -1,11 +1,11 @@
 // Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número". Para saber o tipo de dado você pode usar o operador `typeof`
 
-let  content = ""
+let  content = []
 
-if (typeof content === Number) {
-  console.log('é um número')
-} else {
+if ( typeof content === (String)) {
   console.log('não é um número')
+} else {
+  console.log('é um número')
 }
 
-content = 0
+content = ['a']
